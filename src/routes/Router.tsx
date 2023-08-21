5ab24c52206b14c4c05eab4e9b41beca646a69b7
gbdev03 <gbdev03@gbringel.com>
2023-08-21 13:16:59 -0400

    CI: github actions

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+11111111111import { Route, Routes } from "react-router-dom";
 import { Login } from "@pages/general/Login";
 import { RoutersPathName } from "@/routes/schemas.ts";
 
 export function Router() {
     return (
         <Routes>
+            <Route
+                path={"/projeto-web-base"}
+                element={<Login />}
+            />
             <Route
                 path={"/"}
                 element={<Login />}
